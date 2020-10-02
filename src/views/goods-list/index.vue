8dd8989a4cf49cba09218f26036e172604016d65
yukang <18790336164@163.com>
2020-10-02 18:23:10 +0800

    开发中 完成商品列表页

--- src/views/goods-list/index.vue
+++ src/views/goods-list/index.vue
@@ -14,20 +14,127 @@
           </ul>
         </div>
       </div>
+      <div class="el-card header">
+        <ul>
+          <li
+            v-for="(item, index) in type"
+            :key="index"
+            :class="[{ active: index === current }]"
+            @click="handleChange(index)"
+          >
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+      <div class="el-card box clearfix">
+        <div
+          v-for="(item, index) in goodList"
+          :key="index"
+          class="fl goods-item"
+        >
+          <GoodsItem :model="item"></GoodsItem>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import Search from "../home/components/search.vue";
-
+  import GoodsItem from "@/components/goods-item.vue";
   export default {
     name: "GoodsList",
     components: {
       Search,
+      GoodsItem,
     },
     data() {
       return {
+        current: 0,
+        type: ["综合", "销量", "价格", "人气"],
+        goodList: [
+          {
+            img: require("@/assets/imgs/goods3.png"),
+            title: "麻婆豆腐川味麻婆豆腐",
+            assess: "1678",
+            collect: 2,
+            money: 15.6,
+            sell: 213,
+          },
+          {
+            img: require("@/assets/imgs/goods4.png"),
+            title: "麻婆豆腐川味麻婆豆腐",
+            assess: "1678",
+            collect: 2,
+            money: 15.6,
+            sell: 213,
+          },
+          {
+            img: require("@/assets/imgs/goods5.png"),
+            title: "麻婆豆腐川味麻婆豆腐",
+            aassess: "1678",
+            collect: 2,
+            money: 15.6,
+            sell: 213,
+          },
+          {
+            img: require("@/assets/imgs/goods3.png"),
+            title: "麻婆豆腐川味麻婆豆腐",
+            assess: "1678",
+            collect: 2,
+            money: 15.6,
+            sell: 213,
+          },
+          {
+            img: require("@/assets/imgs/goods4.png"),
+            title: "麻婆豆腐川味麻婆豆腐",
+            assess: "1678",
+            collect: 2,
+            money: 15.6,
+            sell: 213,
+          },
+          {
+            img: require("@/assets/imgs/goods5.png"),
+            title: "麻婆豆腐川味麻婆豆腐",
+            assess: "1678",
+            collect: 2,
+            money: 15.6,
+            sell: 213,
+          },
+          {
+            img: require("@/assets/imgs/goods4.png"),
+            title: "麻婆豆腐川味麻婆豆腐",
+            assess: "1678",
+            collect: 2,
+            money: 15.6,
+            sell: 213,
+          },
+          {
+            img: require("@/assets/imgs/goods5.png"),
+            title: "麻婆豆腐川味麻婆豆腐",
+            assess: "1678",
+            collect: 2,
+            money: 15.6,
+            sell: 213,
+          },
+          {
+            img: require("@/assets/imgs/goods4.png"),
+            title: "麻婆豆腐川味麻婆豆腐",
+            assess: "1678",
+            collect: 2,
+            money: 15.6,
+            sell: 213,
+          },
+          {
+            img: require("@/assets/imgs/goods5.png"),
+            title: "麻婆豆腐川味麻婆豆腐",
+            assess: "1678",
+            collect: 2,
+            money: 15.6,
+            sell: 213,
+          },
+        ],
+
         list: [
           {
             title: "分类",
@@ -51,7 +158,11 @@
       };
     },
     created() {},
-    methods: {},
+    methods: {
+      handleChange(e) {
+        this.current = e;
+      },
+    },
   };
 </script>
 <style lang="scss" scoped>
@@ -85,6 +196,48 @@
         }
       }
       // @include justify();
+    }
+    .header {
+      margin: 10px 0;
+      color: $colorC;
+      .active {
+        background: $green;
+        color: #fff;
+      }
+      ul {
+        @include center-flex(y);
+        li {
+          cursor: pointer;
+          width: 139px;
+          height: 51px;
+          line-height: 51px;
+          text-align: center;
+          position: relative;
+          &::after {
+            content: "";
+            height: 25px;
+            width: 1px;
+            background: #ccc;
+            display: block;
+            position: absolute;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            margin: auto;
+          }
+        }
+      }
+    }
+    .box {
+      padding: 30px 30px 0 30px;
+      margin-bottom: 10px;
+      > .goods-item {
+        margin-right: 50px;
+        &:nth-child(5n) {
+          margin-right: 0;
+        }
+        margin-bottom: 50px;
+      }
     }
   }
 </style>
