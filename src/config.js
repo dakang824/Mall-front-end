1986d9184211d93b68b14eb55794c5951b836e5e
e <18790336164@163.com>
2020-10-31 15:17:52 +0800

    开发中 修改配置文件

--- src/config.js
+++ src/config.js
@@ -1,9 +1,19 @@
 /*
  * @Author: yukang 1172248038@qq.com
  * @Date: 2020-09-20 20:10:07
- * @LastEditTime: 2020-10-25 18:23:36
+ * @LastEditTime: 2020-10-31 14:34:20
  */
 module.exports = {
+  // 开发以及部署时的URL 如"/test/"
+  publicPath: "/",
+  // 生产环境构建文件的目录名
+  outputDir: "dist",
+  // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+  assetsDir: "static",
+  // 开发环境每次保存时是否输出为eslint编译警告
+  lintOnSave: true,
+  //开发环境端口号
+  devPort: "80",
   // @token在Cookie中存储的天数，默认1天
   cookieExpires: 30,
   // 默认的接口地址 如果是开发环境和生产环境走vab-mock-server，当然你也可以选择自己配置成需要的接口地址
@@ -28,7 +38,7 @@ module.exports = {
   //消息框消失时间
   messageDuration: 3000,
   //标题 （包括初次加载雪花屏的标题 页面的标题 浏览器的标题）
-  title: "大康小站",
+  title: "嗨厨玩味",
   //token存储位置localStorage sessionStorage cookie
   storage: "localStorage",
   //token在localStorage、sessionStorage、cookie存储的key的名称
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,37 @@
 /*
  * @Author: yukang 1172248038@qq.com
  * @Date: 2020-10-09 23:07:12
- * @LastEditTime: 2020-10-17 19:49:19
+ * @LastEditTime: 2020-10-31 15:02:09
  */
+const path = require("path");
+const CompressionPlugin = require("compression-webpack-plugin");
+const WebpackBar = require("webpackbar");
+const {
+  publicPath,
+  title,
+  assetsDir,
+  outputDir,
+  lintOnSave,
+  devPort,
+} = require("./src/config");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
+  publicPath,
+  assetsDir,
+  outputDir,
+  lintOnSave,
   devServer: {
-    port: 80,
+    hot: true,
+    port: devPort,
+    open: true,
+    noInfo: false,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
     disableHostCheck: true,
     proxy: {
       "/service": {
@@ -13,5 +39,24 @@ module.exports = {
         changeOrigin: true,
       },
     },
+  },
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+        "^": resolve("src/views"),
+      },
+    },
+    plugins: [
+      new WebpackBar({ name: title }),
+      // new CompressionPlugin({
+      //   test: /\.js$|\.css$|\.html$/,
+      //   filename: "[path].gz[query]",
+      //   algorithm: "gzip",
+      //   minRatio: 0.8,
+      //   threshold: 8192,
+      // }),
+    ],
   },
 };
